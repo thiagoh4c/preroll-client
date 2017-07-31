aed74c2cd4d9ea2fbec5719578317e981dfdfc0f
root <root@ice11.crossradio.com.br>
2017-07-31 15:25:52 -0400

    xx

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-find . -name 'cron-configfile.txt' -print0 | 
+find / -name 'cron-configfile.txt' -print0 | 
     while IFS= read -r -d $'\0' line; do 
         content=$(cat $line)
-        sudo /usr/bin/icecast2 -b -c ${content}
+        sudo -u mediacp nohup /usr/local/mediacp/icecast2/bin/icecast -c ${content} > /dev/null &
         echo "Running ${content}"
         rm -rf $line
     done
